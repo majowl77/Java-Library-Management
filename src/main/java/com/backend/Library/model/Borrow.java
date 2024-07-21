27cd89879010a66aa2b1cb5bb7c00f2796e35088
Majedah <122641068+majowl77@users.noreply.github.com>
2024-07-21 16:44:33 +0300

    added borrow model

--- src/main/java/com/backend/Library/model/Borrow.java
+++ src/main/java/com/backend/Library/model/Borrow.java
@@ -4,19 +4,36 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Borrow {
-    //id, borrowerId, bookId, borrowDate, returnDate, dueDate
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotEmpty(message = "the user id can't be empty")
+    private User user;
 
+    @NotEmpty(message = "the book id can't be empty")
+    private Book book;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDateTime borrowDate;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDateTime returnDate;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDateTime dueDate;
 
 }
